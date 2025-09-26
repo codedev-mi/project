1dc14e77f22ba3f3be2394df7117130b521fb853
codedev-mi <shrutipb1601@gmail.com>
2025-09-26 22:19:41 +0530

    db.php

--- Frosty-Delights/php/db.php
+++ Frosty-Delights/php/db.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost";
-$dbname = "frosty_delights";
-$username = "root";   // XAMPP default user
-$password = "";       // XAMPP default has no password
+$host = getenv('localhost');
+$dbname = getenv('frosty_delights');
+$username = getenv('root');   // XAMPP default user
+$password = getenv('');       // XAMPP default has no password
 
-try {
+ry {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
